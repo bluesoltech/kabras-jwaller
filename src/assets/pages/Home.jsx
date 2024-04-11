de0c1e7dc9d46edd54fdd2b0e34390ab5574503c
hardiks7 <hardipsinhp780@gmail.com>
2024-04-11 18:37:11 +0530

    add banner

--- src/assets/pages/Home.jsx
+++ src/assets/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import kabras from '../img/kabra 123-01.jpg'
 
 const Home = () => {
   return (
-    <div className='text-[100px]'>banner 
-
+    <div className='h-screen w-screen p-10 bg-black'>
+      <img className='rounded-2xl h-fit xl:h-[800px] w-full' src={kabras} alt="kabras" />
+      <h1 className='  text-center  text-[#D5AA5D] xl:text-7xl'>coming soon</h1>
     </div>
   )
 }
