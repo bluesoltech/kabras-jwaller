62fa4dec41b13f5870eea5e94fe135be94906723
hardiks7 <hardipsinhp780@gmail.com>
2024-04-12 11:14:47 +0530

    fevicone

--- src/assets/pages/Home.jsx
+++ src/assets/pages/Home.jsx
@@ -4,8 +4,7 @@ import kabras from '../img/kabra 123-01.jpg'
 const Home = () => {
   return (
     <div className='h-screen w-screen p-10 bg-black'>
-      <img className='rounded-2xl h-fit xl:h-[800px] w-full' src={kabras} alt="kabras" />
-      <h1 className='  text-center  text-[#D5AA5D] xl:text-7xl'>coming soon</h1>
+      <img className='rounded-2xl h-fix xl:h-[910px] w-full' src={kabras} alt="kabras" />
     </div>
   )
 }
