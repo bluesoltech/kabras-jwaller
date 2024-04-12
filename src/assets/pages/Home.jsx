ec084622f57c3cac6c854125849507313ebffaf8
hardiks7 <hardipsinhp780@gmail.com>
2024-04-12 11:27:20 +0530

    some changes on bg

--- src/assets/pages/Home.jsx
+++ src/assets/pages/Home.jsx
@@ -3,7 +3,7 @@ import kabras from '../img/kabra 123-01.jpg'
 
 const Home = () => {
   return (
-    <div className='h-screen w-screen p-10 bg-black'>
+    <div className='h-screen w-screen p-10 bg-gradient-to-r from-white  to-red-200'>
       <img className='rounded-2xl h-fix xl:h-[910px] w-full' src={kabras} alt="kabras" />
     </div>
   )
